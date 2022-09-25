118ebc7c5598a3235d1f5b6852e0c6fefc4eeb11
ANDRII HALYTSKYI <galickyav@gmail.com>
2022-09-25 19:10:43 +0300

    011

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,31 @@
-import {useState,useEffect} from "react";
-import { nanoid } from 'nanoid';
+import { useDispatch,useSelector } from "react-redux";
 import PropTypes from 'prop-types';
 import Form from './Form';
 import ContactList from "./ContactList";
 import Filter from "./Filter";
+import { addContact,changeFilter,deleteContact } from '../redux/store';
 
 
 
-function App(){
-
-  const [contacts, setContacts] = useState(()=>JSON.parse(localStorage.getItem('contacts')) ?? []);
-  const [filter, setFilter] = useState('');
-
-   useEffect(() => {
-    window.localStorage.setItem('contacts', JSON.stringify(contacts))
-   }, [contacts])
-
-
+function App() {  
+  const dispatch = useDispatch();
+  const contacts = useSelector(state => state.contacts.items);
+  const filter = useSelector(state => state.contacts.filter);
+  const onDeleteContact = name => dispatch(deleteContact(name));
   
-  const deleteContact = idContact => {
-    setContacts(contacts.filter(contact => contact.id !== idContact));
-      };
-
-
-  const formSubmitHandler = data => {
-    const contact = {
-      id: nanoid(),
-      name: data.name,
-      number: data.number,
-    };
+  const formSubmitHandler = data => {   
 
     if (contacts.find(contact => contact.name.toLowerCase() === data.name.toLowerCase())) {
       alert(`${data.name} is already in contacts`);
       return;
     };
-
-    setContacts(prevState => [contact, ...prevState]
-    );
+    
+    dispatch(addContact(data));
+    
   };
 
-  const changeFilter = event => {
-    setFilter(event.currentTarget.value);    
+  const changerFilter = event => {
+    dispatch(changeFilter(event.currentTarget.value));     
    };
 
   
@@ -50,16 +35,14 @@ function App(){
   );
 
     return (
-      <div>
+      <div>     
         <h1>Phonebook</h1>
         <Form onSubmit={formSubmitHandler}/>        
         <h2>Contacts</h2>
-        <Filter value={filter} onChange={changeFilter}/>
-        <ContactList contacts={filtredContacts} onDeleteContact={deleteContact}/>
+        <Filter value={filter} onChange={changerFilter}/>
+        <ContactList filtredContacts={filtredContacts} onDeleteContact={onDeleteContact}/>
       </div>
     )
-  
-
 };
 
 App.propTypes = {
